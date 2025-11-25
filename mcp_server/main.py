27343726d2f0f0e22d38d2edad83859ebd6ead60
Alexey Grigorev <alexeygrigorev@users.noreply.github.com>
2025-11-25 14:06:49 +0000

    mcp

---
+++ mcp_server/main.py
@@ -0,0 +1,41 @@
+import requests 
+
+from fastmcp import FastMCP
+from search_tools import SearchTools
+from minsearch import AppendableIndex
+
+
+def initialize_index():
+    docs_url = 'https://github.com/alexeygrigorev/llm-rag-workshop/raw/main/notebooks/documents.json'
+    docs_response = requests.get(docs_url)
+    documents_raw = docs_response.json()
+
+    documents = []
+
+    for course in documents_raw:
+        course_name = course['course']
+
+        for doc in course['documents']:
+            doc['course'] = course_name
+            documents.append(doc) 
+
+    index = AppendableIndex(
+        text_fields=["question", "text", "section"],
+        keyword_fields=["course"]
+    )
+
+    index.fit(documents)
+    return index
+
+
+index = initialize_index()
+tools = SearchTools(index)
+
+mcp = FastMCP("Search 🚀")
+
+mcp.tool(tools.search)
+mcp.tool(tools.add_entry)
+
+
+if __name__ == "__main__":
+    mcp.run(transport="sse")
